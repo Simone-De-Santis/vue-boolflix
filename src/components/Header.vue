a5fa17c86f86baa37ee54af090e0ac5a7987333a
Simone-De-Santis <simone.agentelazio@yahoo.it>
2021-09-10 14:20:59 +0200

    prendo il valore dall'imput

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,30 @@
 <template>
-  <h1>questo è l'header dove metteremo un logo e un (componente search)</h1>
+  <nav class="navbar navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand">Navbar</a>
+      <div class="d-flex">
+        <input
+          v-model.trim="userSearch"
+          type="text"
+          class="w-100"
+          placeholder="Cerca o inizia una nuova chat..."
+        />
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
 export default {
   name: "Header",
+  data() {
+    return {
+      userSearch: "",
+    };
+  },
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
 </style>
