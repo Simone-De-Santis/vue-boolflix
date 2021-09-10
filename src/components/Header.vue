156ec97ec245973461f3f33eadad53f56cfa5593
Simone-De-Santis <simone.agentelazio@yahoo.it>
2021-09-10 12:23:52 +0200

    aggancio componenti header e main

---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>questo è l'header dove metteremo un logo e un (componente search)</h1>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,15 @@
+<template>
+  <h5>
+    questo è l'header dove verra inserito un container contenenti il (componente
+    card da stampare)
+  </h5>
+</template>
+
+<script>
+export default {
+  name: "Main",
+};
+</script>
+
+<style>
+</style>
