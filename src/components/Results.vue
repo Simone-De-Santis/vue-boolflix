455aa4ef20937598d60f2497cd2744b2c6031842
Simone-De-Santis <simone.agentelazio@yahoo.it>
2021-09-14 16:56:12 +0200

    restailing card

---
+++ src/components/Results.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <h2>{{ title }}</h2>
+    <section :id="id" class="row row-cols-1 row-cols-md-4 g-4">
+      <div v-for="item in items" :key="item.id">
+        <Cardss :item="item" />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import Cardss from "@/components/Cardss.vue";
+export default {
+  name: "Results",
+  components: {
+    Cardss,
+  },
+  props: ["id", "title", "items"],
+};
+</script>
+
+<style>
+</style>
